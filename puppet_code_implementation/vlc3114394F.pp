90d14e429c52be13a046e3fb372533d12b4ff62d
AngKokLeong <ang.kok.leong.development@gmail.com>
2023-12-26 08:30:42 +0000

    added package name attribute in package block and enabled in service block

--- puppet_code_implementation/vlc3114394F.pp
+++ puppet_code_implementation/vlc3114394F.pp
@@ -3,12 +3,14 @@
 class vlc3114394F {
     
     package { 'VLC Media Player':
+      name => 'vlc',
       ensure => "latest",
       provider => "apt"
     }
 
     service { 'vlc_service':
       ensure => "running",
+      enabled => 'true',
       require => Package['VLC Media Player']
     }
 }
