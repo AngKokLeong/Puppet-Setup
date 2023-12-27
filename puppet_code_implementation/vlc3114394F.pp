fc4f5a9fb1e95de6f608045d6fd2595d4f09332c
AngKokLeong <ang.kok.leong.development@gmail.com>
2023-12-27 11:50:13 +0000

    added command to modify the vlc binary to allow root user to execute vlc service

--- puppet_code_implementation/vlc3114394F.pp
+++ puppet_code_implementation/vlc3114394F.pp
@@ -5,15 +5,19 @@ class vlc3114394F {
     package { 'VLC Media Player':
       name => 'vlc',
       ensure => "latest",
-      provider => "apt"
+      provider => "apt",
+      before => Exec["Change the command for vlc"]
+    }
+
+    exec { 'Change the command for vlc':
+      command => ["/bin/bash", "sed -i 's/geteuid/getppid/' /usr/bin/vlc"],
+      provider => 'shell',
+      require => Package['VLC Media Player']
     }
 
     service { 'vlc_service':
       ensure => "running",
       enable => 'true',
-      require => Package['VLC Media Player']
+      after => Exec["Change the command for vlc"]
     }
 }
-
-
-
