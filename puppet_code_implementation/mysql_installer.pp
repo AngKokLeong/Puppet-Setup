d0411c746f8e13ebaea0bd51ed90277fd6492132
AngKokLeong <ang.kok.leong.development@gmail.com>
2023-12-27 12:09:35 +0000

    added mysql_installer class

---
+++ puppet_code_implementation/mysql_installer.pp
@@ -0,0 +1,24 @@
+
+class mysql_installer {
+
+  package { 'mysql-ubuntu':
+    name => 'mysql-server',
+    ensure => 'latest',
+    provider => apt,
+    before => Service['mysql-ubuntu-service']
+  }
+
+  service { 'mysql-ubuntu-service':
+    ensure => 'running',
+    enable => true,
+    require => Package['mysql-ubuntu']
+  }
+
+  exec { 'execute-sql-command':
+    # execute the shell script
+    command => ['/bin/bash', 'mysql --user=user3114394F --password=password3114394F db3114394F'],
+    provider => shell,
+    after => Service['mysql-ubuntu-service']
+  }
+  
+}
