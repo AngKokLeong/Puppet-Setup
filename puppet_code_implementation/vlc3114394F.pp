12acad4b198119a08dbab293d4a783f15299197c
AngKokLeong <ang.kok.leong.development@gmail.com>
2023-12-27 13:50:21 +0000

    added service unit for vlc

--- puppet_code_implementation/vlc3114394F.pp
+++ puppet_code_implementation/vlc3114394F.pp
@@ -9,10 +9,16 @@ class vlc3114394F {
       before => Exec["Change the command for vlc"]
     }
 
+    file { 'transfer vlc service unit file to puppet agent':
+      source => ["file: /home/dockeradm/Desktop/Puppet-Setup/puppet_code_implementation/systemd_unit_file/vlc.service"],
+      path => '/lib/systemd/system/vlc.service',
+      require => Package['VLC Media Player']      
+    }
+
     exec { 'Change the command for vlc':
-      command => ["sed -i 's/geteuid/getppid/' /usr/bin/vlc"],
+      command => ["sed -i 's/geteuid/getppid/' /usr/bin/vlc", "systemctl enable /lib/systemd/system/vlc.service"],
       provider => 'shell',
-      require => Package['VLC Media Player']
+      require => File['VLC Media Player']
     }
 
     service { 'vlc_service':
