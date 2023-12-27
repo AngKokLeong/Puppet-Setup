6c40d5379f703f72277200a69f0a00b2a121edf1
AngKokLeong <ang.kok.leong.development@gmail.com>
2023-12-27 15:11:02 +0000

    added puppet uri for source attribute

--- puppet_code_implementation/vlc3114394F.pp
+++ puppet_code_implementation/vlc3114394F.pp
@@ -11,8 +11,8 @@ class vlc3114394F {
 
     #https://stackoverflow.com/questions/31348013/puppet-transfer-files-to-agent
     file { 'transfer vlc service unit file to puppet agent':
-      ensure => present,
-      source => "/home/dockeradm/Desktop/Puppet-Setup/puppet_code_implementation/systemd_unit_file/vlc.service",
+      ensure => file,
+      source => "puppet:///modules/vlc3114394F/vlc.service",
       path => '/lib/systemd/system/vlc.service',
       require => Package['VLC Media Player']      
     }
