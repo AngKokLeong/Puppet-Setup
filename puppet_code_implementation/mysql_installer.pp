5bda04d2cbcfb9e094b3cd4aea7610099a460943
AngKokLeong <ang.kok.leong.development@gmail.com>
2023-12-28 07:39:54 +0000

    uses heredocs in command parameter to execute multiple sql statement

--- puppet_code_implementation/mysql_installer.pp
+++ puppet_code_implementation/mysql_installer.pp
@@ -17,11 +17,13 @@ class mysql_installer {
 
   exec { 'execute-sql-command':
     # execute the shell script
-    command => [
-      'mysql -e "CREATE USER ''user3114394F''@''localhost'' IDENTIFIED BY ''password3114394F''" ', 
-      'mysql -e "CREATE DATABASE db3114394F;"',
-      'mysql -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, INDEX, DROP, ALTER, CREATE TEMPORARY TABLES, LOCK TABLES ON db3114394F.* TO ''user3114394F''@''localhost''; "'
-    ],
+    command => @("MYSQL Commands"/L)
+      mysql \ 
+      -e "CREATE USER 'user3114394F'@'localhost' IDENTIFIED BY 'password3114394F';", \
+      -e "CREATE DATABASE db3114394F;", \
+      -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, INDEX, DROP, ALTER, CREATE TEMPORARY TABLES, LOCK TABLES ON db3114394F.* TO 'user3114394F'@'localhost';"
+      |-"MYSQL Commands"
+    ,
     provider => shell,
     require => Service['mysql-ubuntu-service']
   }
