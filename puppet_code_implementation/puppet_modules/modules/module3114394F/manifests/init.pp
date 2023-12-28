ab6f06de056d0ce65ad33003071ce311db307511
AngKokLeong <ang.kok.leong.development@gmail.com>
2023-12-28 14:29:53 +0000

    add puppet_module folder to store module folder structure

---
+++ puppet_code_implementation/puppet_modules/modules/module3114394F/manifests/init.pp
@@ -0,0 +1,5 @@
+
+class module3114394F {
+  
+
+}
---
+++ puppet_code_implementation/puppet_modules/modules/vlc3114394F/manifests/vlc3114394F.pp
@@ -0,0 +1,33 @@
+# class definition
+
+class vlc3114394F {
+    
+    package { 'VLC Media Player':
+      name => 'vlc',
+      ensure => "latest",
+      provider => "apt",
+      before => Exec["Change the command for vlc"]
+    }
+
+    #https://stackoverflow.com/questions/31348013/puppet-transfer-files-to-agent
+    file { 'transfer vlc service unit file to puppet agent':
+      ensure => file,
+      source => 'puppet:///modules/vlc3114394F/vlc.service',
+      path => '/lib/systemd/system/vlc.service',
+      require => Package['VLC Media Player'],
+      recurse => true,
+      mode => '0777'     
+    }
+
+    exec { 'Change the command for vlc':
+      command => ["sed -i 's/geteuid/getppid/' /usr/bin/vlc", "systemctl enable /lib/systemd/system/vlc.service"],
+      provider => 'shell',
+      require => File['transfer vlc service unit file to puppet agent']
+    }
+
+    service { 'vlc_service':
+      ensure => "running",
+      enable => 'true',
+      require => Exec["Change the command for vlc"]
+    }
+}
