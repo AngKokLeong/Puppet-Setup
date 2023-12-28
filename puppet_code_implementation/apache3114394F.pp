8f808bb78c7859ee951592dc094b35b954765a9e
AngKokLeong <ang.kok.leong.development@gmail.com>
2023-12-28 13:22:25 +0000

    created apache3114394 puppet manifest file to install apache server

---
+++ puppet_code_implementation/apache3114394F.pp
@@ -0,0 +1,22 @@
+
+class apache3114394F {
+
+  package { 'Apache Server':
+    name => 'apache2',
+    ensure => 'installed'
+  }
+
+  service { 'Apache Server Service':
+    name => 'apache2',
+    ensure => running,
+    enable => true,
+    require => Package["Apache Server"]
+  }
+
+  file { 'deploy html file':
+    path => '/var/www/html/app3114394F/index.html',
+    ensure => 'file',
+    content => file('module3114394F/index.html'),
+    require => Service["Apache Server Service"]
+  }
+}
