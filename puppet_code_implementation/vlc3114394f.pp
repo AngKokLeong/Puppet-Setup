d0108be69d5166ab7a5a3686d3c676040ee9fe2f
AngKokLeong <ang.kok.leong.development@gmail.com>
2023-12-29 01:14:27 +0000

    modified the class name to lower case alphabet and added onlyif parameter to prevent wrong execution with the separation of two different linux command to ensure each exec block only execute one linux command

--- puppet_code_implementation/vlc3114394f.pp
+++ puppet_code_implementation/vlc3114394f.pp
@@ -1,6 +1,6 @@
 # class definition
 
-class vlc3114394F {
+class vlc3114394f {
     
     package { 'VLC Media Player':
       name => 'vlc',
@@ -11,23 +11,31 @@ class vlc3114394F {
 
     #https://stackoverflow.com/questions/31348013/puppet-transfer-files-to-agent
     #https://stackoverflow.com/questions/73082615/setting-up-a-systemd-service-to-loop-videos-using-vlc-on-a-raspberry-pi 
-    file { 'transfer vlc service unit file to puppet agent':
+    file { 'transfer-vlc-service-unit-file-to-puppet-agent':
       ensure => file,
       path => '/lib/systemd/system/vlc.service',
       require => Package['VLC Media Player'],
       content => "[Unit]\nDescription=vlc.service\n\n[Service]\nUser=root\nEnvironment='DISPLAY=:0'\nExecStart=/usr/bin/vlc\nWorkingDirectory=/\n\n[Install]\nWantedBy=default.target"
     }
 
-    exec { 'Change the command for vlc':
-      command => ["sed -i 's/geteuid/getppid/' /usr/bin/vlc", "systemctl enable /lib/systemd/system/vlc.service"],
+    exec { 'change-the-permission-for-vlc-binary':
+      command => ["sed -i 's/geteuid/getppid/' /usr/bin/vlc"],
       provider => 'shell',
-      require => File['transfer vlc service unit file to puppet agent']
+      onlyif => "test -e /usr/bin/vlc",
+      require => File['transfer-vlc-service-unit-file-to-puppet-agent']
+    }
+
+    exec { 'enable-vlc-service-in-systemd':
+      command => ["systemctl enable /lib/systemd/system/vlc.service"],
+      provider => 'shell',
+      onlyif => "test -e /lib/systemd/system/vlc.service",
+      require => Exec["change-the-permission-for-vlc-binary"]
     }
 
     service { 'vlc_service':
       name => 'vlc',
       ensure => "running",
       enable => 'true',
-      require => Exec["Change the command for vlc"]
+      require => Exec["enable-vlc-service-in-systemd"]
     }
 }
