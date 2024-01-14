a9405adc7fd5d3238da94830932d4497c712fd33
AngKokLeong <ang.kok.leong.development@gmail.com>
2024-01-14 15:03:46 +0000

    removed the sudo command due to this script will be executed using sudo

--- ubuntu_os_puppet_setup/setup.bash
+++ ubuntu_os_puppet_setup/setup.bash
@@ -13,7 +13,7 @@ apt-get install net-tools git -yq
 # Setup Docker's apt repository
 apt-get install ca-certificates curl gnupg -yq
 install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo \
